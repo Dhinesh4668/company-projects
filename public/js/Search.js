4981c9f186246a38bf2d3999f921ae37670eee3b
Dhinesh Kumar <87540180+Dhinesh4668@users.noreply.github.com>
2023-02-02 22:22:49 +0530

    main

---
+++ public/js/Search.js
@@ -0,0 +1,21 @@
+myFunction() {
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("myTable");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+      td = tr[i];
+      if (td) {
+        txtValue = td.textContent || td.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
+      }       
+    }
+  }
+
+
+  console.log("main");
